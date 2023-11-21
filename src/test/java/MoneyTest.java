f739efee2d7d9e2fd2fad277cd650fe200ca4375
taehyun <93398875+tanggu01@users.noreply.github.com>
2023-11-22 04:27:29 +0900

    :test_tube: update test to handle Dollar side-effect

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -8,7 +8,9 @@ public class MoneyTest {
     public void testMultiplication() {
         Dollar five = new Dollar(5);
         five.times(2);
-        assertEquals(10, five.amount);
+        assertEquals(10, product.amount);
+        five.times(3);
+        assertEquals(15, product.amount);
     }
 }
 
