7ea5baa645d85ef7b089924eec5924a6622840c2
taehyun <93398875+tanggu01@users.noreply.github.com>
2023-11-22 04:12:43 +0900

    :recycle: Remove duplicate of 5 * 2

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -13,13 +13,13 @@ public class MoneyTest {
 }
 
 class Dollar {
-    int amount = 10;
+    int amount;
 
     Dollar(int amount) {
 
     }
 
     void times(int multiplier) {
-
+        amount = 5 * 2;
     }
 }
