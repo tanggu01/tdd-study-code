1a60a5c52fb6a4a346ff4bafea14cce653dde8c9
taehyun <tanggu01@connect.hku.hk>
2023-12-06 19:43:12 +0900

    :sparkles: make static method in `Money` that returns `Dollar`

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -30,6 +30,11 @@ public class MoneyTest {
 
 class Money {
     protected int amount;
+
+    public static Dollar dollar(int amount) {
+        return new Dollar(amount);
+    }
+
     public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount
