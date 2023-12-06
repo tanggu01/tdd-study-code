db4e2eb1055c8a17b9142222faed31109fbe4cd1
taehyun <tanggu01@connect.hku.hk>
2023-12-06 20:38:08 +0900

    :recycle: amend `equals()` to compare currency not the class

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -26,7 +26,7 @@ class Money {
     public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount
-                && getClass().equals(money.getClass());
+                && currency().equals(money.currency());
     }
 
     public String toString() {
