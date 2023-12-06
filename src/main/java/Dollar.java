4cf00dfbe822d4acf03c231ab4f6b1982bc0f239
taehyun <tanggu01@connect.hku.hk>
2023-12-06 20:04:02 +0900

    :recycle: move class package

---
+++ src/main/java/Dollar.java
@@ -0,0 +1,11 @@
+class Dollar extends Money {
+
+    Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    Money times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+
+}
---
+++ src/main/java/Money.java
@@ -0,0 +1,19 @@
+abstract class Money {
+    protected int amount;
+
+    abstract Money times(int multiplier);
+
+    public static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+    public static Money franc(int amount) {
+        return new Franc(amount);
+    }
+
+
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount
+                && getClass().equals(money.getClass());
+    }
+}
