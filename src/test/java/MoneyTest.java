ba7d4ec5a5ba213db898f75a9e996362608478d2
taehyun <tanggu01@connect.hku.hk>
2023-12-06 18:59:00 +0900

    :white_check_mark: add equals() test for Franc

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -22,6 +22,8 @@ public class MoneyTest {
     public void testEquality() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(6).equals(new Dollar(5)));
+        assertTrue(new Franc(5).equals(new Franc(5)));
+        assertFalse(new Franc(6).equals(new Franc(5)));
     }
 }
 
