a1f44efc8b38e9ec2c12df0f8c063f967fc69051
taehyun <taehyun@storelink.io>
2023-12-27 18:51:53 +0900

    :sparkles: introduce Pair and Hashtable rates to store rates

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,4 +1,8 @@
+import java.util.Hashtable;
+
 public class Bank {
+
+    private Hashtable rates = new Hashtable();
 
     Money reduce(Expression source, String to) {
         return source.reduce(this, to);
@@ -7,4 +11,24 @@ public class Bank {
     int rate(String from, String to) {
         return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
     }
+
+    private class Pair {
+        private String key;
+        private String value;
+
+        public Pair(String key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public boolean equals(Object object) {
+            Pair pair = (Pair) object;
+            return key.equals(pair.key) && value.equals(pair.value);
+        }
+
+        public int hashCode() {
+            return 0;
+        }
+    }
+
 }
