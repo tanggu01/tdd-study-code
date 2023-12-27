ddaefc914d70840e4fbe5301fc1b83738ec1c900
taehyun <taehyun@storelink.io>
2023-12-27 17:33:38 +0900

    :test_tube: modify testSimpleAddition to apply the term `Expression` and `reduce`

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import java.beans.Expression;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MoneyTest {
@@ -27,8 +29,11 @@ public class MoneyTest {
 
     @Test
     public void testSimpleAddition() {
-        Money sum = Money.dollar(5).plus(Money.dollar(5));
-        assertEquals(Money.dollar(10), sum);
+        Money five = Money.dollar(5);
+        Expression sum = five.plus(five);
+        Bank bank = new Bank();
+        Money reduced = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(10), reduced);
     }
 
 }
