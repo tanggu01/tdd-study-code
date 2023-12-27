1f2990957f32095efd127fb780d5a99a54c614ac
taehyun <taehyun@storelink.io>
2023-12-27 17:22:55 +0900

    :test_tube: add test for simple dollar addition

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -25,5 +25,11 @@ public class MoneyTest {
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
 
+    @Test
+    public void testSimpleAddition() {
+        Money sum = Money.dollar(5).plus(Money.dollar(5));
+        assertEquals(Money.dollar(10), sum);
+    }
+
 }
 
