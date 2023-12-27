51b1742a9a83ab9ec03e60b010946b24595cce27
taehyun <taehyun@storelink.io>
2023-12-27 18:48:12 +0900

    :test_tube: test fails; add test to test equality of the same elements in two array

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -67,6 +67,11 @@ public class MoneyTest {
         assertEquals(Money.dollar(1), result);
     }
 
+    @Test
+    public void testArrayEquals(){
+        assertEquals(new Object[] {"abc"}, new Object[] {"abc"});
+    }
+
 
 }
 
